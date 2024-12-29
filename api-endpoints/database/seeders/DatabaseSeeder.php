1f9aed457525cb557ec6b85897f439f435334cf9
Liliana Barbosa <liliana.barbosa@udea.edu.co>
2024-12-29 01:21:48 +0100

    Filling the Seeder with Data

--- api-endpoints/database/seeders/DatabaseSeeder.php
+++ api-endpoints/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\CartItem;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use App\Models\ProductVariant;
+use App\Models\ShoppingCart;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +19,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Product::factory(20)->create();
+
+        ProductVariant::factory(200)->create();
+
+        User::factory(15)->create();
+
+        Order::factory(200)->create();
+
+        ShoppingCart::factory(200)->create();
+
+        OrderItem::factory(300)->create();
+        
+        CartItem::factory(300)->create();
+
+
     }
 }
